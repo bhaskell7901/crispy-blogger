5094a34dc8c9ff2bb11667bf030198acd01653aa
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-02-08 09:59:54 -0800

    update server file with session, sequelize andhandlebars requirements

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const path = require('path');
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// const hdbrs = exphbs.create({ helpers });
+
+const currentSession = {
+  secret: 'add something here',
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(currentSession));
+
+app.engine('handlebars', hdbrs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
