d60a6bf6333d2f33677c37e66c5c702d0d37c82b
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-02-08 08:33:07 -0800

    add index files to controllers

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const blog = require('./blog');
+const dashboard = require('./dashboard');
+const comment = require('./comment');
+
+router.use('/blog', blog);
+router.use('/dashboard', dashboard);
+router.use('/comment', comment);
+
+module.exports = router;
