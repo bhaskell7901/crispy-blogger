de14e4994099eb050b56d7e3c6f02eeb75f9b3e2
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-02-10 09:02:50 -0800

    update account create and dashboard views

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./home');
+const dashboard = require('./dashboard');
 const apiRoutes = require('./api');
 
 router.use('/', homeRoutes)
+router.use('/dashboard', dashboard);
 router.use('/api', apiRoutes);
 
 module.exports = router;
